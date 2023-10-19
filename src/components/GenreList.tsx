985aab1ea63a9061f8dd67c855e3036279a5c8a2
Ariel Kuzminski <ariel.kuzminski@gmail.com>
2023-10-19 08:50:48 +0200

    Filtering platforms

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -8,6 +8,7 @@ import {
 } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
+import { Platform } from "../hooks/usePlatforms";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
