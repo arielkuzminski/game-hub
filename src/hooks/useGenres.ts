849024b1c8c0e1be9e145fbedc0b5e0593c1fcdd
Ariel Kuzminski <ariel.kuzminski@gmail.com>
2023-10-24 11:04:57 +0200

    Add react query into project

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,11 +1,26 @@
 import genres from "../data/genres";
 
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
+
 export interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
 
-const useGenres = () => ({ data: genres, isLoading: false, error: null });
+// const useGenres = () => ({ data: genres, isLoading: false, error: null });
+const useGenres = () =>
+  useQuery({
+    queryKey: ["genres"],
+    queryFn: () =>
+      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    staleTime: Infinity,
+    initialData: {
+      count: 24 * 60 * 60 * 1000,
+      results: genres,
+    },
+  });
 
 export default useGenres;
